155d02c5989a743bf97b594aa156e0afe0c51722
DanP1925 <a20110645@pucp.pe>
2017-12-16 14:32:52 -0500

    Agrega migracion de Organizacion

---
+++ AsesoriaMujer/database/migrations/2017_12_16_180148_create_organizacions_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOrganizacionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('organizacions', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('titulo',250);
+            $table->string('ubicacion',250);
+            $table->string('telefono',20);
+            $table->string('encargado',150);
+            $table->time('comienzoAtencion');
+            $table->time('finAtencion');
+            $table->string('paginaWeb',250);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('organizacions');
+    }
+}
