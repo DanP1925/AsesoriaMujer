a4a4a03648a05550fe7dff4e572fe9170a456339
Jehu Pacheco <jehu.pacheco@tektonlabs.com>
2017-12-17 10:14:20 -0500

    nuevo banner y sidebar

--- AsesoriaMujer/resources/views/layout/sidebar.blade.php
+++ AsesoriaMujer/resources/views/layout/sidebar.blade.php
@@ -1,13 +1,12 @@
 <ul class="side-nav" id="nav-mobile">
-  <li>
+  <li class="purple" style="padding-bottom: 20px;">
     <div class="user-view center-align">
-      <img src="{{asset('img/logo_morado.png')}}" class="circle" style="margin: 0 auto; width: 80px; height: 80px">
-      <span class="purple-text name bold">
+      <img src="{{asset('img/logo.png')}}" class="circle" style="margin: 0 auto; width: 80px; height: 80px">
+      <h5 class="white-text name bold">
         AconsejaTEC
-      </span>
+      </h5>
     </div>
   </li>
-  <li><div class="divider"></div></li>
   <li><a href="{{url('/')}}">Home</a></li>
   <li><a href="{{url('organizaciones')}}">Organizaciones</a></li>
   <li><a href="{{url('servicios')}}">Servicios</a></li>
