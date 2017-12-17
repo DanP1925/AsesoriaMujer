12a383f22a29f51c8a5f3f5f1c4333116230fbf1
Jehu Pacheco <jehu.pacheco@tektonlabs.com>
2017-12-17 03:02:21 -0500

    Logo

--- AsesoriaMujer/resources/views/layout/navbar.blade.php
+++ AsesoriaMujer/resources/views/layout/navbar.blade.php
@@ -4,6 +4,9 @@
   </a>
   <div class="container">
     <div class="nav-wrapper">
+      <a href="{{url('')}}" class="brand-logo right" style="width: 30px">
+        <img src="{{asset('img/logo.png')}}" class="responsive-img" alt="">
+      </a>
       <a class="page-title" style="text-transform: uppercase;font-weight: bold;">
         @yield('title')
       </a>
