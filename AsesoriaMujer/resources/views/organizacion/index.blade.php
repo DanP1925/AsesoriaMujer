df93ef5234f5b8d8c11fe26f24e219d2bc667263
DanP1925 <a20110645@pucp.pe>
2017-12-16 23:45:57 -0500

    Organizaciones Controller

--- AsesoriaMujer/resources/views/organizacion/index.blade.php
+++ AsesoriaMujer/resources/views/organizacion/index.blade.php
@@ -1,6 +1,12 @@
- <h1>Organizaciones</h1>
+<h1>Organizaciones</h1>
 <ul>
 @foreach ($organizaciones as $organizacion)
-    <li>{{ $organizacion->titulo}}</li>
+    <div>
+    <a href="/organizaciones/{{ $organizacion->id}}">
+        {{ $organizacion->titulo}}
+    </a>
+    {{ $organizacion->telefono}}
+    {{ $organizacion->ubicacion}}
+    </div>
 @endforeach
 </ul>
